07b9bf78cf2e79b623a9200060bd556b8a38cdd7
Your Name <javiermonterroso14@gmail.com>
2025-12-12 11:49:15 -0600

    Columnas que representan Enteros

--- Proyecto1/limpieza.py
+++ Proyecto1/limpieza.py
@@ -44,3 +44,71 @@ for col in cols_porcentajes:
     df[col] = df[col].fillna(mediana_valida)
 
 df.head(15)
+
+
+# Columnas que representan números enteros
+
+# horas_estudio
+df.loc[df['horas_estudio'] < 0, 'horas_estudio'] = np.nan
+
+df['horas_estudio'] = df['horas_estudio'].fillna(0).astype(int)
+
+
+# cursos_reprobados
+
+# Marcar valores negativos como NaN
+df.loc[df['cursos_reprobados'] < 0, 'cursos_reprobados'] = np.nan
+
+# Llenar faltantes con 0
+df['cursos_reprobados'] = df['cursos_reprobados'].fillna(0).astype(int)
+
+
+
+# actividades_extracurriculares
+
+# Convertir valores vacíos o NaN a lista vacía
+df['actividades_extracurriculares'] = df['actividades_extracurriculares'].replace(
+    ["", "[]", None],
+    pd.NA
+)
+
+# Recorrer fila por fila y corregir
+for i in range(len(df)):
+    value = df.at[i, 'actividades_extracurriculares']
+
+    # Caso 1: valor vacío o NaN → []
+    if pd.isna(value):
+        df.at[i, 'actividades_extracurriculares'] = []
+        continue
+
+    # Caso 2: intentar convertir desde string
+    try:
+        parsed = ast.literal_eval(str(value))
+
+        # Si el resultado es lista → dejarla
+        if isinstance(parsed, list):
+            df.at[i, 'actividades_extracurriculares'] = parsed
+        else:
+            df.at[i, 'actividades_extracurriculares'] = []
+
+    except:
+        # Caso 3: cualquier error → []
+        df.at[i, 'actividades_extracurriculares'] = []
+
+
+# reportes_disciplinarios
+# Marcar valores negativos como NaN
+df.loc[df['reportes_disciplinarios'] < 0, 'reportes_disciplinarios'] = np.nan
+
+# Llenar faltantes con 0
+df['reportes_disciplinarios'] = df['reportes_disciplinarios'].fillna(0).astype(int)
+
+
+# Columna de riesgo
+df['riesgo'] = df['riesgo'].astype(str).str.strip().str.lower()
+
+# Filtrar valores válidos
+df = df[df['riesgo'].isin(['riesgo', 'no riesgo'])].copy()
+
+# Reemplazar directamente riesgo por 0/1
+df['riesgo'] = df['riesgo'].map({'no riesgo': 0, 'riesgo': 1})
